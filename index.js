4018337294b932c2e152aa8b18b1de40e5bb1444
John Johnson Mbeba <john.mbeba@student.moringaschool.com>
2022-08-02 13:35:18 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const main = document.querySelector("main#main");
+main.remove();
+
+let newHeader = document.querySelector("h1#victory");
+newHeader.innerHTML = "YOUR-NAME is the champion";
